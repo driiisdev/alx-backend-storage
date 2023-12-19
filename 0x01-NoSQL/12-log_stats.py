d3828c716be795b5d092114ac3dc64de6e8960cd
driiisdev <olayinkayakub01@gmail.com>
2023-12-19 01:26:04 +0100

    nosql

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This script provides some stats about about nginx logs stored in MongoDB
+"""
+
+import pymongo
+
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+
+    log_stats = '{} logs\nMethods:'.format(nginx.count_documents({}))
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        log_stats += '\n\tmethod {}: {}'.format(method,
+                                                  nginx.count_documents({
+                                                      "method": method
+                                                    }))
+    log_stats += '\n{} status check'.format(nginx.count_documents({
+                                                            "method": "GET",
+                                                            "path": "/status"
+                                                        }))
+    print(log_stats)
---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+This module provides the function `insert_school`
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    This is a funciton that inserts a new
+    document in a collection base on kwargs
+    """
+    if mongo_collection is None or kwargs == {} or kwargs is None:
+        return
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
